a90f04b3c770054913f61a6ad058bde2996c915c
Varun Chengal <tovarunchengal@gmail.com>
2025-01-03 12:16:21 +0530

    Update BookviewAdmin.jsx

--- src/components/BookviewAdmin.jsx
+++ src/components/BookviewAdmin.jsx
@@ -32,8 +32,8 @@ console.log(bookView)
       <>
       <Navbartop/>
       <div className='row'> 
-           <div className='col-2 bg-sbar'><Adminsidebar/></div>
-        <div className='col-10'>
+           <div className='col-3 col-md-2 bg-sbar'><Adminsidebar/></div>
+        <div className='col-9 col-md-10'>
           <div className='modal-ct'>
            <Table>
           
