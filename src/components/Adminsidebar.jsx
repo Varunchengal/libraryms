b416fb54d10e80c7cc5e205474062051cdc740f1
Varun Chengal <tovarunchengal@gmail.com>
2025-01-03 13:27:25 +0530

    Update Adminsidebar.jsx

--- src/components/Adminsidebar.jsx
+++ src/components/Adminsidebar.jsx
@@ -8,11 +8,11 @@ export default function Adminsidebar(){
     return(
         <div>
             <div className='sidebar-text mt-5 ps-1'>
-                <Link className='sidebar-link' to={'/admin'}>Dashboard</Link> <br />
-                <Link className='sidebar-link' to={'/admin/books'}>View Books</Link> <br />
-                <Link className='sidebar-link' to={'/admin/add'}>Add Book</Link> <br />
-                <Link className='sidebar-link' to={'/admin/view'}>View Students</Link> <br />
-                <Link className='sidebar-link' to={'/admin/history'}>View Reservation</Link> <br />
+                <Link className='sidebar-link mb-5' to={'/admin'}>Dashboard</Link> <br /><br />
+                <Link className='sidebar-link' to={'/admin/books'}>View Books</Link> <br /><br />
+                <Link className='sidebar-link' to={'/admin/add'}>Add Book</Link> <br /><br />
+                <Link className='sidebar-link' to={'/admin/view'}>View Students</Link> <br /><br />
+                <Link className='sidebar-link' to={'/admin/history'}>View Reservation</Link> <br /><br />
                 <Link className='sidebar-link' to={'/admin/edit'}>Edit Profile</Link> <br />
             </div>
         </div>
